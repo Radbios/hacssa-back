a5a807673a0689380471bdc0ef1c352efda90087
Fabiano <radbios@gmail.com>
2023-06-14 19:39:16 -0300

    Product: CRUD criado

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Http\Resources\ProductResource;
+use App\Http\Requests\StoreUpdateProductRequest;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $data = Product::all();
+
+        return response()->json($data);
+    }
+
+    public function store(StoreUpdateProductRequest $request)
+    {
+        $data = Product::create($request->all());
+
+        return new ProductResource($data);
+    }
+
+    public function show(string $id)
+    {
+        $data = Product::findOrFail($id);
+
+        return new ProductResource($data);
+    }
+
+    public function update(StoreUpdateProductRequest $request, string $id)
+    {
+        $data = Product::findOrFail($id);
+        $data->update($request->all());
+
+        return new ProductResource($data);
+    }
+
+    public function destroy(string $id)
+    {
+        $data = Product::findOrFail($id);
+        $data->delete();
+
+        return new ProductResource($data);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\Api\LoginController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return "OK";
 });
 
-Route::get('/login', [LoginController::class, 'login']);
+
+Route::apiResource('/products', ProductController::class);
 
