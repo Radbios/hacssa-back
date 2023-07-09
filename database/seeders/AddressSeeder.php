66e704ba0c885f2e628b1f4e9e5b5d6c8bbad125
radbios <radbios@gmail.com>
2023-07-09 15:09:42 -0300

    seeder address and client

---
+++ database/seeders/AddressSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Address;
+
+
+class AddressSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Address::create([
+            'client_id' => 1,
+            'street' => 1,
+            'city' => 1,
+            'state' => 1,
+            'country' => 1,
+            'latitude' =>-9.55742266181595,
+            'longitude' =>-35.74943337986463
+        ]);
+
+        Address::create([
+            'client_id' => 1,
+            'street' => 1,
+            'city' => 1,
+            'state' => 1,
+            'country' => 1,
+            'latitude' =>-9.555878,
+            'longitude' =>-35.751842
+        ]);
+    }
+}
---
+++ database/seeders/ClientSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Client;
+
+
+class ClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Client::create([
+            'user_id' => 1,
+            'name' => 'Fabiano',
+            'phone' => 123,
+            'gender' => 'male',
+        ]);
+    }
+}
